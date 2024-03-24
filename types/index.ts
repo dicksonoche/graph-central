9dcbb4edb20d99bc17daad0c59469821010970d8
simon <simonoche987@gmail.com>
2024-03-24 12:43:31 +0100

    structure

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    containerStyles?: string;
+    handleClick?: 
+    MouseEventHandler<HTMLButtonElement>;
+}
+/* Interface speccifies how a specific structure should look like. What variables and values it should have*/
